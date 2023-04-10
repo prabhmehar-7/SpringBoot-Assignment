6b43bf32be18ec68fb5f7043bbceb26b51fd1cb4
prabhmehar-7 <prabhmehar.chhabra@tothenew.com>
2023-04-10 15:31:34 +0530

    Second

--- src/main/java/com/springboot/assignment2/RestApiAssignment2/question1/Question1.java
+++ src/main/java/com/springboot/assignment2/RestApiAssignment2/question1/Question1.java
@@ -3,6 +3,7 @@ package com.springboot.assignment2.RestApiAssignment2.question1;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
@@ -23,5 +24,13 @@ public class Question1 {
     public String helloInternationalization(){
         Locale locale = LocaleContextHolder.getLocale() ;
         return messageSource.getMessage("hello.message",null,"Error",locale);
+    }
+
+    //Question2
+    @GetMapping(path = "hello-username/{username}")
+    public String helloUsername(@PathVariable String username){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("welcome.message",null,
+                "Default Message",locale)+" "+ username;
     }
 }
